937f7a2e8f5c8ec57e93a09e4bd91fb0fa2b5841
Konstantin Shpinev <ks@microimpuls.com>
2017-01-14 18:55:16 +0500

    clean docs

--- clean_all.sh
+++ clean_all.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-rm -rf smarty-billing-api-docs/*
-rm -rf smarty-tvmw-api-docs/*
+git rm -rf smarty-billing-api-docs/*
+git rm -rf smarty-tvmw-api-docs/*
+mkdir -p smarty-billing-api-docs
+mkdir -p smarty-tvmw-api-docs
+git add *
+git commit -a -m "clean docs"

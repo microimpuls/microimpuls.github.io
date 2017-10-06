1bb15205484185dcdb33a2ac57dbb392c325a5b3
Konstantin Shpinev <ks@microimpuls.com>
2017-10-06 23:09:24 +0500

    update docs

--- justify_make_all.sh
+++ justify_make_all.sh
@@ -3,6 +3,8 @@ MW_PORTALS_DIR=../../git/mw_portals
 CWD=$(pwd)
 
 rm -rf justify-engine-main-jsdoc/*
+rm -rf justify-impuls-main-jsdoc/*
+rm -rf justify-impuls-screens-jsdoc/*
 
 rm -rf $MW_PORTALS_DIR/docs/out
 cd $MW_PORTALS_DIR/docs
@@ -14,3 +16,5 @@ jsdoc -c impuls.screens.json
 cd $CWD
 
 cp -R $MW_PORTALS_DIR/docs/out/engine/main/ justify-engine-main-jsdoc
+cp -R $MW_PORTALS_DIR/docs/out/impuls/main/ justify-impuls-main-jsdoc
+cp -R $MW_PORTALS_DIR/docs/out/impuls/screens/ justify-impuls-screen-jsdoc

226d1227fa0d7e3abca367812876cb4e0035b177
Konstantin Shpinev <ks@microimpuls.com>
2018-05-28 00:26:12 +0500

    clean docs

--- smarty_clean_all.sh
+++ smarty_clean_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git rm -rf smarty-billing-api-docs/*
 git rm -rf smarty-tvmw-api-docs/*
+git rm -rf smarty-viewstats-api-docs/*
 mkdir -p smarty-billing-api-docs
 mkdir -p smarty-tvmw-api-docs
+mkdir -p smarty-viewstats-api-docs
 git add *
 git commit -a -m "clean docs"
--- smarty_make_all.sh
+++ smarty_make_all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apidoc -i ../../git/smarty/tvmiddleware/ -o smarty-tvmw-api-docs/
 apidoc -i ../../git/smarty/billing/ -o smarty-billing-api-docs/
+apidoc -i ../../git/smarty/viewstats/ -o smarty-viewstats-api-docs/

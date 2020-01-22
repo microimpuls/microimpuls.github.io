ad6925c927ee141240a52c62435f3db8e3a46fa5
GitLab Runner <gitlab-runner@tserver>
2020-01-22 14:23:40 +0300

    update docs

--- smarty-device-monitoring-api-docs/api_project.js
+++ smarty-device-monitoring-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-01-20T19:45:37.520Z",
+    "time": "2020-01-22T11:23:39.729Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- smarty-tvmw-api-docs/api_project.js
+++ smarty-tvmw-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-01-20T19:45:33.502Z",
+    "time": "2020-01-22T11:23:35.925Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

627b3725bfbb80d72ba2b6f48d1bf4dcf753598e
GitLab Runner <gitlab-runner@tserver>
2020-03-20 08:37:47 +0300

    update docs

--- smarty-viewstats-api-docs/api_project.js
+++ smarty-viewstats-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-17T11:39:58.430Z",
+    "time": "2020-03-20T05:37:45.963Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

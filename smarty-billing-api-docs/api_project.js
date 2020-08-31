9be8f6cac866f2826b6c1f85725d716e419ead04
GitLab Runner <gitlab-runner@tserver>
2020-08-31 15:34:54 +0300

    update docs

--- smarty-billing-api-docs/api_project.js
+++ smarty-billing-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-31T12:00:48.766Z",
+    "time": "2020-08-31T12:34:52.121Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- smarty-viewstats-api-docs/api_project.js
+++ smarty-viewstats-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-31T12:00:50.028Z",
+    "time": "2020-08-31T12:34:53.406Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

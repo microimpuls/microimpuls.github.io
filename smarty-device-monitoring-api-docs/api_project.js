009e858c7951f3953b2d9df8499f32b3305dbba3
GitLab Runner <gitlab-runner@tserver>
2020-09-11 01:54:42 +0300

    update docs

--- smarty-device-monitoring-api-docs/api_project.js
+++ smarty-device-monitoring-api-docs/api_project.js
@@ -1,21 +1,21 @@
 define({
   "name": "Smarty Device Monitoring API",
   "version": "1.0.0",
-  "description": "Интерфейс взаимодействия абонентских устройств с сервером мониторинга устройств по протоколу HTTP.",
+  "description": "Интерфейс взаимодействия абонентских устройств с сервером Microimpuls MVision по протоколу HTTP.",
   "title": "Smarty Device Monitoring API",
   "url": "http://smarty.example.com/device_monitoring/api",
   "header": {
-    "content": "<h2>Описание</h2>\n<p>Интерфейс <strong>Device Monitoring API</strong> используется для сбора метрик с устройств и рассылки команд провижининга.\nВызовы функций API выполняются через HTTP POST-запросы, параметры передаются в теле запроса в формате JSON.</p>\n<p>Формат ответа также является JSON.</p>\n<p>Подробную информацию о Smarty можно узнать в <a href=\"http://mi-smarty-docs.readthedocs.io\">документации</a>.</p>\n"
+    "content": "<h2>Описание</h2>\n<p>Интерфейс <strong>Device Monitoring API</strong> используется для сбора метрик с устройств и рассылки команд провижининга.\nВызовы функций API выполняются через HTTP POST-запросы, параметры передаются в теле запроса в формате JSON.</p>\n<p>Формат ответа также является JSON.</p>\n"
   },
   "footer": {
-    "content": "<p>© 2019 ООО «Майкроимпульс», <a href=\"http://www.microimpuls.com\">www.microimpuls.com</a></p>\n"
+    "content": "<p>© 2020 ООО «Майкроимпульс», <a href=\"http://www.microimpuls.com\">www.microimpuls.com</a></p>\n"
   },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-08T14:04:48.409Z",
+    "time": "2020-09-10T22:54:14.240Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

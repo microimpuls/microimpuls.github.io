01a7b5647de6809ecf54894dd801fdea95ccd17f
git <dev@microimpuls.com>
2021-08-26 09:14:33 +0300

    update docs

--- smarty-billing-api-docs/api_project.js
+++ smarty-billing-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-25T08:42:22.588Z",
+    "time": "2021-08-26T06:14:29.292Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- smarty-device-monitoring-api-docs/api_project.js
+++ smarty-device-monitoring-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-08-25T08:42:24.990Z",
+    "time": "2021-08-26T06:14:31.526Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

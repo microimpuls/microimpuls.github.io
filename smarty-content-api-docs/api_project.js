939fe4ade549dae91ff4f49b1a74a8f0e3aabcd4
git <dev@microimpuls.com>
2021-10-06 11:41:26 +0300

    update docs

--- smarty-content-api-docs/api_project.js
+++ smarty-content-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-06T06:49:08.634Z",
+    "time": "2021-10-06T08:41:25.599Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- smarty-viewstats-api-docs/api_project.js
+++ smarty-viewstats-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-06T06:49:06.561Z",
+    "time": "2021-10-06T08:41:23.379Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

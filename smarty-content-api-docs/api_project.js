179b426902ca7443079c1a21d9c773e047fada8f
git <dev@microimpuls.com>
2021-11-22 16:23:53 +0300

    update docs

--- smarty-content-api-docs/api_project.js
+++ smarty-content-api-docs/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-11-22T09:00:25.027Z",
+    "time": "2021-11-22T13:23:53.208Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

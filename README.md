7f96a9eda918683300a645843db75671f9c0f8be
Alexander Larin <alarin@microimpuls.com>
2023-10-25 00:53:03 +0500

    Updated index.html

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # microimpuls.github.io
 Some Microimpuls products documentation
-
-Generate Smarty TV-Middleware API doc:
-``
-    apidoc -i /Path/To/Smarty/tvmiddleware -o smarty-tvmw-api-docs
-``
-
-Generate Smarty Billing API doc:
-``
-    apidoc -i /Path/To/Smarty/billing -o smarty-billing-api-docs
-``
--- smarty_make_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apidoc -i ../../git/smarty/tvmiddleware/ -o smarty-tvmw-api-docs/
-apidoc -i ../../git/smarty/billing/ -o smarty-billing-api-docs/
-apidoc -i ../../git/smarty/viewstats/ -o smarty-viewstats-api-docs/
-apidoc -i ../../git/smarty/device_monitoring/ -o smarty-device-monitoring-api-docs/
